9edf9b8f95d9d51425a5a7b2ba97caf196d005e5
lwdlouis <lwdlouis@sohu.com>
2020-11-21 19:59:30 +0800

    init data-backup component

---
+++ ds-data-backup/src/main/java/com/dml/ds/databackup/DsDataBackupApplication.java
@@ -0,0 +1,25 @@
+package com.dml.ds.databackup;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsDataBackupApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		try {
+			SpringApplication.run(DsDataBackupApplication.class, args);
+			System.exit(0);
+		} catch (Exception e) {
+			System.exit(1);
+		}
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		if (1 == 1) {
+			throw new RuntimeException("hahahah");
+		}
+	}
+}
---
+++ ds-data-backup/src/test/java/com/dml/ds/databackup/DsDataBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dml.ds.databackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsDataBackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
