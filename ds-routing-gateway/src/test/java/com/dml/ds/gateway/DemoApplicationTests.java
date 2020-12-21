32154d06179a154ab82f66b5e1fb80d28c29d070
lwdlouis <lwdlouis@sohu.com>
2020-12-22 07:16:17 +0800

    add optional gateway, springcloud gateway

---
+++ ds-routing-gateway/src/test/java/com/dml/ds/gateway/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dml.ds.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
