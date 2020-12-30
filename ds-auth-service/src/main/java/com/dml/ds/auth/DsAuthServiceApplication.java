82eb40754aeb909570633c597b232cbb0c786482
lwdlouis <lwdlouis@sohu.com>
2020-12-30 21:45:38 +0800

    Add auth service module

---
+++ ds-auth-service/src/main/java/com/dml/ds/auth/DsAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dml.ds.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAuthServiceApplication.class, args);
+	}
+
+}
---
+++ ds-auth-service/src/test/java/com/dml/ds/auth/DsAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dml.ds.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
